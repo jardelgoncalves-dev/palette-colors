b7900e598129e5900514b4f96cdab6fb0139f136
jardelgoncalves <jardelgoncalves1996@gmail.com>
2021-09-12 13:14:30 -0300

    feat: add colors interface

---
+++ src/interfaces/theme.ts
@@ -0,0 +1,21 @@
+export type Colors = {
+  primary: {
+    main: string
+    hover: string
+    active: string
+  }
+
+  neutral: {
+    black: string
+    white: string
+  }
+
+  grayscale: {
+    paragraph: string
+  }
+
+  card: {
+    background: string
+    paragraph: string
+  }
+}
