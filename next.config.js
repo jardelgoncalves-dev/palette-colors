06cff3bc6898b54c7cfdeb8e5317229b41e57f9a
jardelgoncalves <jardelgoncalves1996@gmail.com>
2021-09-12 14:54:05 -0300

    chore: config compose plugin ad next images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withPlugins = require('next-compose-plugins')
+const withImages = require('next-images')
+
+const plugins = [[withImages]]
+
+const nextConfiguration = {
   reactStrictMode: true,
+  images: {
+    disableStaticImages: true,
+  },
 }
+
+module.exports = withPlugins([...plugins], nextConfiguration)
