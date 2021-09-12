ae687e96cd0474b33022377e1a5f9e256615a614
jardelgoncalves <jardelgoncalves1996@gmail.com>
2021-09-12 13:13:45 -0300

    feat: add colors tokens

---
+++ src/styles/tokens/colors.ts
@@ -0,0 +1,20 @@
+import { Colors } from 'interfaces/theme'
+
+export const colors: Colors = {
+  primary: {
+    main: '#5C3BFE',
+    active: '#4223DD',
+    hover: '#856FEE',
+  },
+  grayscale: {
+    paragraph: '#212121',
+  },
+  neutral: {
+    black: '#000',
+    white: '#fff',
+  },
+  card: {
+    background: '#fff',
+    paragraph: '#575757',
+  },
+} as const
