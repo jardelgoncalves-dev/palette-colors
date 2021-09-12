6a795a41f131c5d5cc261fc2c1a9d62a545cb743
jardelgoncalves <jardelgoncalves1996@gmail.com>
2021-09-12 13:13:19 -0300

    feat: add spacings tokens

---
+++ src/styles/tokens/spacings.ts
@@ -0,0 +1,11 @@
+export const spacing = {
+  '2xs': '4px',
+  xs: '8px',
+  sm: '16px',
+  md: '24px',
+  lg: '32px',
+  xl: '48px',
+  '2xl': '64px',
+  '3xl': '88px',
+  '4xl': '122px',
+} as const
