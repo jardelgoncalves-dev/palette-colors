96dcf991f3e764e6df582f75da88b7b28403d6e0
jardelgoncalves <jardelgoncalves1996@gmail.com>
2021-09-12 13:14:45 -0300

    chore: export tokens

---
+++ src/styles/tokens/index.ts
@@ -0,0 +1,7 @@
+export * from './breakpoints'
+export * from './typography'
+export * from './spacings'
+export * from './shapes'
+export * from './layers'
+export * from './colors'
+export * from './sizes'
