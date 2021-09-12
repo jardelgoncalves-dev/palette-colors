da252b458fd0cdecfb637f741d4dd0dbddf57f53
jardelgoncalves <jardelgoncalves1996@gmail.com>
2021-09-12 15:02:43 -0300

    feat: apply settings provider, app contect ad head seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
+import HeadSEO from 'components/core/HeadSEO'
+import AppContext from 'contexts/app.context'
+import { SettingsProvider } from 'contexts/settings.context'
 import type { AppProps } from 'next/app'
 
 function MyApp({ pageProps, Component }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
+  return (
+    <SettingsProvider>
+      <AppContext>
+        <HeadSEO
+          title={pageProps.title}
+          ogTitle={pageProps.ogTitle}
+          metaDescription={pageProps.metaDescription}
+          ogDescription={pageProps.ogDescription}
+        />
+        <Component {...pageProps} />
+      </AppContext>
+    </SettingsProvider>
+  )
 }
 
 export default MyApp
