21327db7f5cd05bbb64c56c7193824d2336490ed
jardelgoncalves <jardelgoncalves1996@gmail.com>
2021-09-12 13:15:16 -0300

    feat: export tokens as theme application

---
+++ src/styles/theme.ts
@@ -0,0 +1,19 @@
+import {
+  breakpoints,
+  typography,
+  spacing,
+  shapes,
+  layers,
+  colors,
+  sizes,
+} from './tokens'
+
+export default {
+  colors,
+  breakpoints,
+  typography,
+  spacing,
+  shapes,
+  layers,
+  sizes,
+} as const
