01b7a0f554fff54e6252e58bc52c0d32ac84792d
jardelgoncalves <jardelgoncalves1996@gmail.com>
2021-09-12 13:47:25 -0300

    chore: add head seo component

---
+++ src/components/core/HeadSEO/HeadSEO.tsx
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+
+type HeadSEOProps = {
+  title?: string
+  ogTitle?: string
+  metaDescription?: string
+  ogDescription?: string
+}
+
+function HeadSEO({
+  title,
+  ogTitle,
+  ogDescription,
+  metaDescription,
+}: HeadSEOProps): JSX.Element {
+  return (
+    <Head>
+      <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
+      {(title || ogTitle) && (
+        <>
+          <meta itemProp="name" content={title || ogTitle} />
+          <meta property="og:title" content={ogTitle || title} />
+          <meta name="twitter:title" content={ogTitle || title} />
+        </>
+      )}
+
+      {(metaDescription || ogDescription) && (
+        <>
+          <meta name="description" content={metaDescription || ogDescription} />
+          <meta
+            itemProp="description"
+            content={metaDescription || ogDescription}
+          />
+          <meta
+            property="og:description"
+            content={ogDescription || metaDescription}
+          />
+          <meta
+            name="twitter:description"
+            content={ogDescription || metaDescription}
+          />
+        </>
+      )}
+    </Head>
+  )
+}
+
+export default HeadSEO
