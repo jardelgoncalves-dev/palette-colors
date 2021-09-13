05d6de5188c5c5e0e68121fe965a8116deb4c398
jardelgoncalves <jardelgoncalves1996@gmail.com>
2021-09-13 00:30:12 -0300

    feat: add header and navbar to application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import HeadSEO from 'components/core/HeadSEO'
 import AppContext from 'contexts/app.context'
 import { SettingsProvider } from 'contexts/settings.context'
+
+import Header from 'components/core/Header'
+import Navbar from 'components/core/Navbar'
+
 import type { AppProps } from 'next/app'
 
 function MyApp({ pageProps, Component }: AppProps): JSX.Element {
@@ -8,11 +12,14 @@ function MyApp({ pageProps, Component }: AppProps): JSX.Element {
     <SettingsProvider>
       <AppContext>
         <HeadSEO
-          title={pageProps.title}
+          title={pageProps.pageTitle}
           ogTitle={pageProps.ogTitle}
           metaDescription={pageProps.metaDescription}
           ogDescription={pageProps.ogDescription}
         />
+        <Header>
+          <Navbar />
+        </Header>
         <Component {...pageProps} />
       </AppContext>
     </SettingsProvider>
