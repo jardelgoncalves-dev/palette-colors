6daeafac9c745f42e6e3acf4330a87571ae0be73
jardelgoncalves <jardelgoncalves1996@gmail.com>
2021-09-13 00:18:36 -0300

    feat: add title tag

--- src/components/core/HeadSEO/HeadSEO.tsx
+++ src/components/core/HeadSEO/HeadSEO.tsx
@@ -18,6 +18,7 @@ function HeadSEO({
       <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
       {(title || ogTitle) && (
         <>
+          <title>{title || ogTitle}</title>
           <meta itemProp="name" content={title || ogTitle} />
           <meta property="og:title" content={ogTitle || title} />
           <meta name="twitter:title" content={ogTitle || title} />
