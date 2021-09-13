363c2c8fb7d9c549a2f21e586eb7a680c5309abe
jardelgoncalves <jardelgoncalves1996@gmail.com>
2021-09-13 00:30:47 -0300

    feat: change page to container page and add miragejs api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,51 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps } from 'next'
+import { createServer, Model } from 'miragejs'
 
-const Home: NextPage = () => {
-  return <h1>Hello</h1>
+createServer({
+  models: {
+    color: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      colors: [
+        {
+          id: 1,
+          colors: [
+            '#ff0000',
+            '#d80404',
+            '#b40000',
+            '#8d0404',
+            '#6d0101',
+            '#530000',
+            '#240000',
+          ],
+          name: 'Vermelhos',
+        },
+      ],
+    })
+  },
+
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/colors', () => {
+      return this.schema.all('color')
+    })
+    this.post('/colors', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('color', data)
+    })
+  },
+})
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {
+      metaDescription: 'Crie grupos com as suas cores favoritas',
+      pageTitle: 'Palette Colors',
+      revalidate: 60 * 60 * 24, // 1 day
+    },
+  }
 }
-
-export default Home
+export { default } from 'containers/Home'
