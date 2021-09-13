f047ee3bca12f069a6a3959ca8e572eedc0b4281
jardelgoncalves <jardelgoncalves1996@gmail.com>
2021-09-13 00:24:10 -0300

    feat: add modal is new colors group handle

--- src/contexts/settings.context.tsx
+++ src/contexts/settings.context.tsx
@@ -4,22 +4,32 @@ import { WithChildren } from 'interfaces/children'
 type SettingContextProps = {
   isBlockScroll: boolean
   setIsBlockScroll: (blocked: boolean) => void
+  isNewColorsGroupOpen: boolean
+  setIsNewColorsGroupOpen: (blocked: boolean) => void
 }
 
 export const defaultValue: SettingContextProps = {
   isBlockScroll: false,
   setIsBlockScroll: () => undefined,
+  isNewColorsGroupOpen: false,
+  setIsNewColorsGroupOpen: () => undefined,
 }
 
 export const SettingsContext = createContext<SettingContextProps>(defaultValue)
 
 export const SettingsProvider = ({ children }: WithChildren): JSX.Element => {
   const [isBlockScroll, setIsBlockScroll] = useState(defaultValue.isBlockScroll)
+  const [isNewColorsGroupOpen, setIsNewColorsGroupOpen] = useState(
+    defaultValue.isNewColorsGroupOpen
+  )
+
   return (
     <SettingsContext.Provider
       value={{
         isBlockScroll,
         setIsBlockScroll,
+        isNewColorsGroupOpen,
+        setIsNewColorsGroupOpen,
       }}
     >
       {children}
